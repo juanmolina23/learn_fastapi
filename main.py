fd2d69ed2c54279d4ceb47ca2d59b44150b5b461
Juan Molina Jiron <jmolinaj@fiu.edu>
2021-04-26 16:39:11 -0400

    Added local db using sqlalchemy and sqlite, added models and schemas for endpoints, created endpoints. Added requirements file to install necessary dependencies.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -18,3 +20,49 @@ def about():
             'page_name': 'About Page'
         }
     }
+
+@app.get('/blog')
+def showBlog(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    
+    if published:
+        return {
+            'data': f'{limit} published blogs from the db'
+        }
+    else:
+        return {
+            'data': f'{limit} blogs from the db'
+        }
+    
+
+@app.get('/blog/unplubished')
+def unpublished():
+    return {
+        'data': {
+            'unplubished_blogs': 'unpublished blogs here'
+        }
+    }
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {
+        'data': {
+            'id': id
+        }
+    }
+
+@app.get('/blog/{id}/comments')
+def showComments(id: int):
+    return {
+        'data': {
+            'id': id
+        }
+    }
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data': 'blog is created!'}
